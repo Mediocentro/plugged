e2cece1605d2fdbe4443901b78d2bb50096c4d7e
J Aditya <aditya.mailboxin1@gmail.com>
2018-03-08 20:36:20 +0530

    Create initFirebase.js
    A JavaScript file that would be used to initialize the Firebase database owned by one of the team members.

---
+++ initFirebase.js
@@ -0,0 +1,13 @@
+<script src="https://www.gstatic.com/firebasejs/4.10.1/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDM6K6poUKbqbzgbsY8b8OFDbTeWmnZCtw",
+    authDomain: "major-project-vaps-nitk.firebaseapp.com",
+    databaseURL: "https://major-project-vaps-nitk.firebaseio.com",
+    projectId: "major-project-vaps-nitk",
+    storageBucket: "major-project-vaps-nitk.appspot.com",
+    messagingSenderId: "232209395587"
+  };
+  firebase.initializeApp(config);
+</script>
