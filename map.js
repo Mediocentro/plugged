9d4993d15d627636d537dfd9c08a32d4b823d665
J Aditya <aditya.mailboxin1@gmail.com>
2018-03-10 16:03:43 +0530

    Update map.js
    Just increasing the zoom setting

--- map.js
+++ map.js
@@ -1,7 +1,7 @@
 function initMap() {
         var myLatLng = {lat: 29.9695, lng: 76.8783};
         var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 4,
+          zoom: 10,
           center: myLatLng
         });
         var marker = new google.maps.Marker({
