1857721dba0f651f96b63d60462b2269ecf9ac59
J Aditya <aditya.mailboxin1@gmail.com>
2018-03-10 17:17:13 +0530

    Update map2.js
    Testing map2

--- map2.js
+++ map2.js
@@ -4,7 +4,7 @@
 
       function initMap() {
         var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 10,
+          zoom: 15,
           center: {lat: -33.9, lng: 151.2}
         });
 
