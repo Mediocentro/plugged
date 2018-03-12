f9e88f94ab54a11b24c5321ef56530d2490bc059
J Aditya <aditya.mailboxin1@gmail.com>
2018-03-13 02:03:36 +0530

    Added Database.js
    Tracks in real time the changes to the database containing meterIDs and meter location information.

---
+++ database.js
@@ -0,0 +1,28 @@
+
+
+var firebaseCoords = firebase.database().ref("/meters");
+var i = 0;
+
+var meters = [];
+firebaseCoords.on('value', function(snapshot){
+	var j = 0;
+	
+	snapshot.forEach(function(data){
+		meters[j] = new Array(4);
+		meters[j][3] = j+1;
+		meters[j][0] = data.child("meterID").val();
+		meters[j][1] = data.child("GPS_lat").val();
+		meters[j][2] = data.child("GPS_lon").val();
+		j++;
+	});
+
+	// for (i = 0; i<meters.length; i++){
+	// 	$("#test").append("<tr><td>" + meters[i][0] + "</td><td>" + meters[i][1] + "</td><td>" 
+	//  	+ meters[i][2] +	"</td></tr>");
+	// }
+	// console.log(meters[1][3]);
+
+	// $("#test").append("<tr><td>" + meterID + "</td><td>" + GPS_lat + "</td><td>" 
+	// 	+ GPS_lon +	"</td></tr>");
+});
+
