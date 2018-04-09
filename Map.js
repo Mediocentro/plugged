0f0cdfb95d79ec1b4fdd58d6d8b8605c447e51c0
J Aditya <aditya.mailboxin1@gmail.com>
2018-04-10 02:14:13 +0530

    Update Map.js
    Formatted the markers in such a way that the required specifications for meters shall be met at least. We should see about the bounce thing, once this has been implemented.

--- Map.js
+++ Map.js
@@ -14,65 +14,28 @@ function initMap(){
        var image = {
           meterOn:{
             url: 'https://maps.google.com/mapfiles/kml/paddle/grn-circle.png',
-            size: new google.maps.Size(20,32),
-            origin: new google.maps.Point(0,0),
-            anchor: new google.maps.Point(0,32)
+            scaledSize: new google.maps.Size(32,32)
           },
           meterOff:{
             url: 'https://maps.google.com/mapfiles/kml/paddle/blu-circle.png',
-            size: new google.maps.Size(20,32),
-            origin: new google.maps.Point(0,0),
-            anchor: new google.maps.Point(0,32)
-          },
+            scaledSize: new google.maps.Size(32,32)
+            },
           meterCritical:{
             url: 'https://maps.google.com/mapfiles/kml/paddle/red-circle.png',
-            size: new google.maps.Size(20,32),
-            origin: new google.maps.Point(0,0),
-            anchor: new google.maps.Point(0,32)
+            scaledSize: new google.maps.Size(32,32)
           },
           nodeOn:{
-            url: 'https://maps.google.com/mapfiles/kml/paddle/grn-blank-lv.png',
-            size: new google.maps.Size(20,32),
-            origin: new google.maps.Point(0,0),
-            anchor: new google.maps.Point(0,32)      
+            url: 'https://maps.google.com/mapfiles/kml/paddle/grn-blank-lv.png'      
           },
           nodeOff:{
-            url: 'https://maps.google.com/mapfiles/kml/paddle/blu-blank-lv.png',
-            size: new google.maps.Size(20,32),
-            origin: new google.maps.Point(0,0),
-            anchor: new google.maps.Point(0,32)      
+            url: 'https://maps.google.com/mapfiles/kml/paddle/blu-blank-lv.png'
           },
           nodeCritical:{
-            url: 'https://maps.google.com/mapfiles/kml/paddle/red-blank-lv.png',
-            size: new google.maps.Size(20,32),
-            origin: new google.maps.Point(0,0),
-            anchor: new google.maps.Point(0,32)      
+            url: 'https://maps.google.com/mapfiles/kml/paddle/red-blank-lv.png'      
           }
        }; 
-
-
-
-
-
-        // //The marker image that would be used in this case (meters) 
-        // var image = {
-        //    url: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
-        //   // This marker is 20 pixels wide by 32 pixels high.
-        //   size: new google.maps.Size(20, 32),
-        //   // The origin for this image is (0, 0).
-        //   origin: new google.maps.Point(0, 0),
-        //   // The anchor for this image is the base of the flagpole at (0, 32).
-        //   anchor: new google.maps.Point(0, 32)
-        // };
+        
        
-        // var shape = {
-        //   coords: [1, 1, 1, 20, 18, 20, 18, 1],
-        //   type: 'poly'
-        // };  
-
-        
-        
-
         var firebaseCoords = firebase.database().ref("/meters");
         
         firebaseCoords.on('value', function(snapshot){
@@ -86,13 +49,13 @@ function initMap(){
           var CoordLon = data.child("GPS_lon").val();
           var CoordCon = data.child("con").val();
           if(CoordCon === 1){
-            var CoordIcon = image['meterOn'];
+            var CoordIcon = image.meterOn;
           }
-          else if(CoordCon === 2){
-            var CoordIcon = image['meterOff'];
+          else if(CoordCon === 0){
+            var CoordIcon = image.meterOff;
           }
           else{
-            var CoordIcon = image['meterCritical'];
+            var CoordIcon = image.meterCritical;
           }
 
           var marker = new google.maps.Marker({
